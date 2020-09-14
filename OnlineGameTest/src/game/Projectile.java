db711272ac247947c147f063fc085889027316c3
traskalex <traska6984@my.uwstout.edu>
2020-09-14 15:22:32 -0500

    Added basics to projectile class

--- OnlineGameTest/src/game/Projectile.java
+++ OnlineGameTest/src/game/Projectile.java
@@ -2,4 +2,43 @@ package game;
 
 public class Projectile {
 
+    private static final int MAX_BOUNCE = 5;
+
+    private float xVel;
+    private float yVel;
+    private int xPos;
+    private int yPos;
+    private int bounceCount;
+
+    public Projectile(int x , int y, float xVel, float yVel) {
+        xPos = x;
+        yPos = y;
+        this.xVel = xVel;
+        this.yVel = yVel;
+    }
+
+    public float getxVel() {
+        return xVel;
+    }
+
+    public float getyVel() {
+        return yVel;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public int getBounceCount() {
+        return bounceCount;
+    }
+
+    public int getMaxBounce() {
+        return MAX_BOUNCE;
+    }
+
 }

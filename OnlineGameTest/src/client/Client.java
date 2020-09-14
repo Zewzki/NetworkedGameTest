55ffd1ca54669cda6c3625e8e28d0c5995ec2f77
traskalex <traska6984@my.uwstout.edu>
2020-09-13 20:22:01 -0500

    added pog

--- OnlineGameTest/src/client/Client.java
+++ OnlineGameTest/src/client/Client.java
@@ -1,4 +1,9 @@
 package client;
 
 public class Client {
+    public static void main(String[] args){
+        for(int Pog = 0; Pog < 1; Pog++)
+            System.out.println("Pog:" + Pog);
+    }
+
 }

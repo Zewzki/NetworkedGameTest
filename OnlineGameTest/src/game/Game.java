83bce5d73525e60439f44bd10095fe0e487500a7
traskalex <traska6984@my.uwstout.edu>
2020-09-14 15:16:22 -0500

    Added basics to Game, Obstacle, and player

--- OnlineGameTest/src/game/Game.java
+++ OnlineGameTest/src/game/Game.java
@@ -20,6 +20,33 @@ public class Game {
 
     }
 
-    public void test() {}
+    public void generateMap() {
+        obstacles.add(new Obstacle(9, 10, 20, 20));
+        obstacles.add(new Obstacle(400, 10, 120, 20));
+        obstacles.add(new Obstacle(400, 300, 50, 120));
+        obstacles.add(new Obstacle(56, 300, 30, 60));
+
+        players.add(new Player(10, 10));
+    }
+
+    public int getWidth() {
+        return WIDTH;
+    }
+
+    public int getHeight() {
+        return HEIGHT;
+    }
+
+    public ArrayList<Obstacle> getObstacles() {
+        return obstacles;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public ArrayList<Projectile> getProjectiles() {
+        return projectiles;
+    }
 
 }
--- OnlineGameTest/src/game/Player.java
+++ OnlineGameTest/src/game/Player.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Player {
 
     private static final int MOVEMENT_SPEED = 5;
@@ -10,11 +12,30 @@ public class Player {
     private int xPos;
     private int yPos;
 
-    public Player() {
-
+    public Player(int x, int y) {
+        xPos = x;
+        yPos = y;
     }
 
-    public void movePlayer() {
+    public void movePlayer(int x, int y, ArrayList<Obstacle> obstacles) {
+        for (int i = 0; i < obstacles.size(); i++) {
+            // right pos of player to left pos of obstacle
+            if (x + PLAYER_WIDTH < obstacles.get(i).getXPos()) {
+                // left pos of player to right pos of obstacle
+                if (x > obstacles.get(i).getXPos() + obstacles.get(i).getWidth()) {
+                    xPos = x;
+                }
+            }
+
+            if (y + PLAYER_HEIGHT < obstacles.get(i).getYPos()) {
+                // left pos of player to right pos of obstacle
+                if (y > obstacles.get(i).getYPos() + obstacles.get(i).getHeight()) {
+                    yPos = y;
+                }
+            }
+
+
+        }
 
     }
 

228093232246b68e19b362dd9786d562485b4157
Michael Wisnewski <mcwtennis@gmail.com>
2020-09-14 14:37:02 -0500

    sync

--- OnlineGameTest/src/game/Game.java
+++ OnlineGameTest/src/game/Game.java
@@ -1,9 +1,25 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Game {
 
-    public Game() {
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 800;
+
+    private int nPlayers;
+
+    private ArrayList<Obstacle> obstacles;
+    private ArrayList<Player> players;
+    private ArrayList<Projectile> projectiles;
+
+    public Game(int nPlayers) {
+
+        this.nPlayers = nPlayers;
+        obstacles = new ArrayList<>();
 
     }
+
+    public void test() {}
 
 }
---
+++ OnlineGameTest/src/game/Player.java
@@ -0,0 +1,21 @@
+package game;
+
+public class Player {
+
+    private static final int MOVEMENT_SPEED = 5;
+
+    private static final int PLAYER_WIDTH = 50;
+    private static final int PLAYER_HEIGHT = 50;
+
+    private int xPos;
+    private int yPos;
+
+    public Player() {
+
+    }
+
+    public void movePlayer() {
+
+    }
+
+}
